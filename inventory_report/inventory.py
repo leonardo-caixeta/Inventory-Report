9232cce828e0726f547ac6dac9ec43906d888e90
leonardo <leonardocaixeta07@gmail.com>
2024-01-25 17:20:48 -0300

    fix: class Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,15 +1,15 @@
-from typing import List
+from typing import Optional
 
 from inventory_report.product import Product
 
 
 class Inventory:
-    def __init__(self, data: List[Product] = None) -> None:
-        self._data = data if data is not None else []
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        self._data = data or []
 
     @property
     def data(self):
         return self._data.copy()
 
-    def add_data(self, data):
+    def add_data(self, data: list[Product]):
         self._data.extend(data)
