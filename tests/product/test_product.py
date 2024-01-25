ab0a903af2890fb3a2cb3682cf45b25507c09f87
leonardo <leonardocaixeta07@gmail.com>
2024-01-25 15:20:21 -0300

    feat: requisito 1 e 2(?)

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        '420',
+        'camomila',
+        "Leo's Chás",
+        '10/01/2024',
+        '10/03/2024',
+        '10203050420',
+        'manter na embalagem'
+    )
+    assert product.id == "420"
+    assert product.product_name == "camomila"
+    assert product.company_name == "Leo's Chás"
+    assert product.manufacturing_date == "10/01/2024"
+    assert product.expiration_date == "10/03/2024"
+    assert product.serial_number == "10203050420"
+    assert product.storage_instructions == "manter na embalagem"
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "420",
+        "camomila",
+        "Leo's Chás",
+        "10/01/2024",
+        "10/03/2024",
+        "10203050420",
+        "manter na embalagem",
+    )
+
+    assert "The product 420 - camomila" in product.__str__()
+    assert "with serial number 10203050420" in product.__str__()
+    assert "manufactured on 10/01/2024" in product.__str__()
+    assert "by the company Leo's Chás" in product.__str__()
+    assert "valid until 10/03/2024" in product.__str__()
+    assert (
+        "must be stored according to the following instructions: manter na embalagem" # NOQA
+        in product.__str__()
+    )
