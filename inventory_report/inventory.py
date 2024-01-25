e2f6333c11c84c92c6504e72e45266cf7633d9ad
leonardo <leonardocaixeta07@gmail.com>
2024-01-25 16:55:43 -0300

    feat: class inventory(?)

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,15 @@
+from typing import List
+
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: List[Product] = None) -> None:
+        self._data = data if data is not None else []
+
+    @property
+    def data(self):
+        return self._data.copy()
+
+    def add_data(self, data):
+        self._data.extend(data)
